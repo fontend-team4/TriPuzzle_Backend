3fa79a1f099da687b443aae2860a1875d1a78ae2
XX <kapritmyrtis@gmail.com>
2024-12-02 23:52:24 +0800

    新增schedule route

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-import express from 'express';
-import passport from 'passport';
-import cors from 'cors';
-import session from 'express-session';
-import dotenv from 'dotenv';
-import usersRouter from './src/routes/users.js';
-import authRoutes from './src/routes/auth.js';
-import './src/configs/passport.js';
+import express from "express";
+import passport from "passport";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+import usersRouter from "./src/routes/users.js";
+import authRoutes from "./src/routes/auth.js";
+import "./src/configs/passport.js";
 dotenv.config();
 
 const app = express();
 
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'your_secret_key',
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/auth', authRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/users', usersRouter);
+app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/users", usersRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
 });
 
 const PORT = process.env.PORT || 3000;
