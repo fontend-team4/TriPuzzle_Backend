1b91e49a1bf0ba9d2197cd92b80c237c21c46787
XX <kapritmyrtis@gmail.com>
2024-12-11 16:16:55 +0800

    MERGE調整

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-import express from 'express';
-import passport from 'passport';
-import cors from 'cors';
-import session from 'express-session';
-import dotenv from 'dotenv';
-import usersRouter from './src/routes/users.js';
-import authRoutes from './src/routes/auth.js';
-import './src/configs/passport.js';
+import express from "express";
+import passport from "passport";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+// import usersRouter from "./src/routes/users.js";
+import authRoutes from "./src/routes/auth.js";
+import "./src/configs/passport.js";
+import { expressjwt } from "express-jwt";
+import { ZodError } from "zod";
+import { router as schedulesRouter } from "./src/routes/schedules.js";
+// import { authenticate } from "./src/middlewares/auth.js";
+// import { authenticator } from "./src/middlewares/authenticator.js";
+import { router as usersRouter } from "./src/routes/users.js";
+import { config } from "./config.js";
+
+const router = express.Router();
 dotenv.config();
 
 const app = express();
 
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'your_secret_key',
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/auth', authRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/users', usersRouter);
+app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/users", usersRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
 });
-
-
-import express from "express";
-import cors from "cors";
-import { expressjwt } from "express-jwt";
-import { ZodError } from "zod";
-import { router as schedulesRouter } from "./src/routes/schedules.js";
-import { authenticate } from "./src/middlewares/auth.js";
-const router = express.Router();
-
-import { authenticator } from "./src/middlewares/authenticator.js";
-import { router as usersRouter } from "./src/routes/users.js";
-import { config } from "./config.js";
-
 
 // CORS 設定
 app.use(
@@ -113,4 +112,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
