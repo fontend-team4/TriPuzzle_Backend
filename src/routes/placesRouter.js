c542819c505d94530a3c982f2adede9c6658a389
Timothy <timothy9am@gmail.com>
2024-12-14 18:20:29 +0800

    API v2

--- src/routes/placesRouter.js
+++ src/routes/placesRouter.js
@@ -1,18 +1,11 @@
 import express from "express";
+import { searchPlaces } from "../controllers/placesController.js";
 import { getRoute, calculateDistances } from "../controllers/mapController.js";
-import {
-  keywordSearch,
-  mapSearch,
-  filterSearch,
-} from "../controllers/placesController.js";
 
 const router = express.Router();
 
+router.get("/search", searchPlaces);
 router.post("/directions", getRoute);
 router.post("/distances", calculateDistances);
-
-router.get("/search", filterSearch);
-router.get("/search2", keywordSearch);
-router.get("/search3", mapSearch);
 
 export default router;
