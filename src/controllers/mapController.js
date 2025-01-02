7c3cb1ee0526d159094a7ff6bc9596ef5b838784
Timothy <timothy9am@gmail.com>
2025-01-02 12:34:15 +0800

    feat: add distance matrix API

--- src/controllers/mapController.js
+++ src/controllers/mapController.js
@@ -1,21 +1,22 @@
 import { getDirections, getDistanceMatrix } from "../services/googleMaps.js";
 
 export const getRoute = async (req, res) => {
-  try {
-    const { origin, destination } = req.body;
-    const directions = await getDirections(origin, destination);
-    res.json(directions);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  const { origin, destination } = req.body;
+  const directions = await getDirections(origin, destination);
+  res.json(directions);
 };
 
 export const calculateDistances = async (req, res) => {
-  try {
-    const { origins, destinations } = req.body;
-    const distances = await getDistanceMatrix(origins, destinations);
-    res.json(distances);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  const { origins, destinations, mode } = req.body;
+  const results = await getDistanceMatrix(origins, destinations, mode);
+  const distances = results.map((row, place) => ({
+    origin: origins[place],
+    elements: row.elements.map((element, place) => ({
+      destination: destinations[place],
+      distance: element.distance?.value || "None",
+      duration: element.duration?.text || "None",
+    })),
+  }));
+
+  res.json(distances);
 };
--- src/controllers/placesController.js
+++ src/controllers/placesController.js
@@ -8,32 +8,26 @@ import {
 export const searchPlaces = async (req, res) => {
   const { city, type, query, latitude, longitude } = req.query;
 
-  try {
-    let location;
-    let placesID;
+  let location;
+  let placesID;
 
-    if (city) {
-      location = await getCoordinates(city);
-    }
-    if (latitude && longitude) {
-      location = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
-    }
-
-    if (query) {
-      placesID = await textSearchPlaces(query, location);
-    }
-    if (type) {
-      placesID = await nearbySearchPlaces(type, location);
-    }
-
-    const placesInfo = await Promise.all(
-      placesID.map(async (place) => await getPlacesInfo(place.place_id))
-    );
-
-    res.json(placesInfo);
-  } catch (error) {
-    res.status(500).json({
-      error: error.response?.data || error.message,
-    });
+  if (city) {
+    location = await getCoordinates(city);
   }
+  if (latitude && longitude) {
+    location = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
+  }
+
+  if (query) {
+    placesID = await textSearchPlaces(query, location);
+  }
+  if (type) {
+    placesID = await nearbySearchPlaces(type, location);
+  }
+
+  const placesInfo = await Promise.all(
+    placesID.map(async (place) => await getPlacesInfo(place.place_id))
+  );
+
+  res.json(placesInfo);
 };
